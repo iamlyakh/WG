51c9256ec013b612a7f961e203670c763ae5a1e0
Alexander Lyakh <lyakh@scand.com>
2017-02-14 16:41:44 +0300

    Added webpack configuration for sass.

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
 var path = require('path');
 var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
 	devtool: 'inline-source-map',
 	entry: [
 		'webpack-hot-middleware/client',
-		'./src/index.js'
+		'./src/index.js',
+		'./src/styles/main.scss'
 	],
 	output: {
 		path: path.resolve(__dirname, 'build'),
@@ -14,14 +16,31 @@ module.exports = {
 	},
 	plugins: [
 		new webpack.optimize.OccurenceOrderPlugin(),
-		new webpack.HotModuleReplacementPlugin()
+		new webpack.HotModuleReplacementPlugin(),
+		new ExtractTextPlugin('styles.css', {
+			allChunks: true
+		})
 	],
 	module: {
-		loaders: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			include: __dirname,
-			loader: 'babel'
-		}]
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				include: __dirname,
+				loader: 'babel'
+			},
+			{
+				test: /\.scss$/,
+				loader: ExtractTextPlugin.extract('style-loader', 'css-loader!resolve-url!sass-loader?sourceMap')
+			},
+			{
+				test: /\.css$/,
+				loader: ExtractTextPlugin.extract('style-loader', 'css-loader')
+			},
+			{
+				test: /\.woff2?$|\.ttf$|\.eot$|\.svg$|\.png|\.jpe?g|\.gif$/,
+				loader: 'file-loader'
+			}
+		]
 	}
 };
